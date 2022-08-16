117690337072e737b5907b0a4ee91ede4e2b92f4
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-16 15:01:59 -0300

    Aula 83
    Validando utilizando o ValidationSummary

--- BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
+++ BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
@@ -6,9 +6,11 @@ namespace BlazorApp_Forms.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [StringLength(10, ErrorMessage = "O nome não pode exceder 10 caracteres")]
         public string Nome { get; set; }
-        [EmailAddress]
+        [Required(ErrorMessage = "O email deve ser informado")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
         public string Email { get; set; }
 
         [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80")]
