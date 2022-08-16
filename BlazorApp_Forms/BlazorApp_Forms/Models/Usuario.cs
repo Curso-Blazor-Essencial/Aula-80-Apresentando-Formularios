ee4e45402888dfc2c26e7e4a6d95625e18835245
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-16 16:50:38 -0300

    Aula 86
    Exercício, trabalhando as tratativas na classe Usuário

--- BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
+++ BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
@@ -4,16 +4,36 @@ namespace BlazorApp_Forms.Models
 {
     public class Usuario
     {
-        public int Id { get; set; }
-
-        [Required(ErrorMessage = "O nome deve ser informado")]
+        [Required(ErrorMessage = "É necessário informar um nome")]
         [StringLength(10, ErrorMessage = "O nome não pode exceder 10 caracteres")]
         public string Nome { get; set; }
-        [Required(ErrorMessage = "O email deve ser informado")]
-        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+
+        [Required(ErrorMessage = "É necessário informar um sobrenome")]
+        [StringLength(10, ErrorMessage = "O nome não pode exceder 10 caracteres")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar uma idade")]
+        [Range(18, 100, ErrorMessage = "A idade deve ser maior que 18 e menor que 100")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar um login")]
+        [MinLength(4, ErrorMessage = "É necessário que o login tenha mais de 4 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar um email")]
+        [EmailAddress(ErrorMessage = "É necessário que o email tenha um formato válido")]
         public string Email { get; set; }
 
-        [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80")]
-        public int Idade { get; set; }
+        [Required(ErrorMessage = "É necessário escolher um perfil")]
+        public string Perfil { get; set; }
+
+        [Required(ErrorMessage = "É necessário informar uma senha")]
+        [MinLength(8, ErrorMessage = "É necessário que a senha tenha pelo menos 8 caracteres")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "É necessário confirmar a senha")]
+        [Compare(nameof(Password), ErrorMessage = "A senha inserida não é a mesma da senha informada")]
+
+        public string ConfirmaPassword { get; set; } = string.Empty;
     }
 }
