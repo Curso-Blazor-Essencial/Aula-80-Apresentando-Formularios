ca6020213d69bd22d47e36a4d842a5be9d8b81c3
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-16 14:51:20 -0300

    Aula 82
    Validação de dados.

--- BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
+++ BlazorApp_Forms/BlazorApp_Forms/Models/Usuario.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorApp_Forms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp_Forms.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Range(18, 80, ErrorMessage = "A idade deve estar entre 18 e 80")]
         public int Idade { get; set; }
     }
 }
